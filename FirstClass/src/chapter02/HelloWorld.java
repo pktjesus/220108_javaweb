c2c50ed803b770951f82c7dab5fb54b1e4ec8250
kitae-park-suwon <pktjesus@gmail.com>
2022-01-08 13:11:17 +0900

    220108첫수업

---
+++ FirstClass/src/chapter02/HelloWorld.java
@@ -0,0 +1,41 @@
+package chapter02;
+
+public class HelloWorld { 	
+	public static void main(String[] args) {		
+		System.out.println("Hello World4");
+		
+		boolean a_;	// 자료형 변수명;
+		a_ = false;	// 변수 = 초기화;
+		int b = 0;	/// 자료형 변수명 = 초기화;
+		byte c = 127;
+		
+		int d1 = 0;
+		int e = 0;
+		int E = 0;
+		
+		int z = 0;
+		int y = 0;
+		int x = z + y;
+		
+		int leftNumber = 1;
+		int left_number = 1;
+		int rightNumber = 2;
+		int twoNumberSum = leftNumber + rightNumber;
+		int two_number_sum = leftNumber + rightNumber;
+		
+		char a = 'A';
+		System.out.println("a:" + a);
+		
+		int bc = a;
+		System.out.println("bc:" + bc);
+		
+		char ch1 = 66;
+		System.out.println("ch1:" + ch1);
+		
+		int d2 = a + ch1;	// 65 + 66
+		System.out.println("d2:" + d2);		// 131
+		
+		// char c1 = 'Hello';	// 에러
+		String str1 = "Hello";
+	}
+}
