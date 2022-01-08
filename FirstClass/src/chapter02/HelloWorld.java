3fb20be6286ca57b978c61ae2299fceba0386880
kitae-park-suwon <pktjesus@gmail.com>
2022-01-08 13:52:44 +0900

    2

--- FirstClass/src/chapter02/HelloWorld.java
+++ FirstClass/src/chapter02/HelloWorld.java
@@ -37,5 +37,47 @@ public class HelloWorld {
 		
 		// char c1 = 'Hello';	// 에러
 		String str1 = "Hello";
+		
+		float f3 = 3.14151515f;		// float일 경우 f는 무조건 입력
+		double d3 = 3.14151515;		// double일 경우 d는 생략 가능
+		
+		System.out.println("float :" + f3);
+		System.out.println("doulbe :" + d3);
+		
+		String name;		// 변수 선언
+		name = "홍길동";		// 변수 초기화
+		
+		String name2 = "홍길동";		// 변수 선언과 동시에 초기화
+		String name3 = null;		// String name3;
+		String name4 = "null";
+		String name5 = "";
+		
+		System.out.println("name2 : " + name2);
+		System.out.println("name3 : " + name3);
+		System.out.println("name4 : " + name4);
+		System.out.println("name5 : " + name5);
+		
+		String name6 = name2;
+		System.out.println("name6 : " + name6);		// 홍길동
+		name2 = "유관순";
+		System.out.println("name6 : " + name6);		// 홍길동
+		System.out.println("name2 : " + name2);		// 유관순
+		
+		String s10 = "1";
+		String s11 = "1.5";
+		String s12 = "true";
+		
+		System.out.println("s10 : " + s10);
+		System.out.println("s11 : " + s11);
+		System.out.println("s12 : " + s12);
+		
+		int i10 = 1;
+		double d10 = 1.1;
+		boolean b10 = true;
+		
+		System.out.println("i10 : " + i10);
+		System.out.println("d10 : " + d10);
+		System.out.println("b10 : " + b10);
+		
 	}
 }
