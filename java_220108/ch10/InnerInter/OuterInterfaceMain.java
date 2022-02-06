fe9cdb08e59e6ada4ea6c773174fded660c4821a
ads1 <dsaf@asdf.com>
2022-02-06 12:54:17 +0900

    10-4

--- java_220108/ch10/InnerInter/OuterInterfaceMain.java
+++ java_220108/ch10/InnerInter/OuterInterfaceMain.java
@@ -1,10 +1,12 @@
 package ch10.InnerInter;
 
 public class OuterInterfaceMain {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		OuterInterface outInter = new OuterInterface();
 
+		outInter.setInterfaceEx(new InterfaceImple());
+		outInter.outerMethod();
+		outInter.setInterfaceEx(new InterfaceImple2());
+		outInter.outerMethod();
 	}
-
 }
